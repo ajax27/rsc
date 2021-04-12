6022d785a82cc813122c0776380f7d5910708ae1
Shaun Collins <shaun.c16@hotmail.co.uk>
2021-04-12 14:24:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='grid-container'>
+      <header>
+        <a href='/'>Ajax27 Shopping</a>
       </header>
+      <main>Product List</main>
+      <footer>
+        <p>&copy; Copyright 2016 - 2021. Ajax27.com All rights reserved</p>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
